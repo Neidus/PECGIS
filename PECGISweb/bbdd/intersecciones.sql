f146040c56c13003324db4fb7354358e6ebc4f78
fernandogarciaferna <36203207+fernandogarciaferna@users.noreply.github.com>
2018-02-27 17:12:15 +0100

    Add files via upload

---
+++ PECGISweb/bbdd/intersecciones.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE public.intersecciones
+(
+  id integer NOT NULL DEFAULT nextval('intersect_id'::regclass),
+  usuario character(20) NOT NULL,
+  geom geometry(LineString,4326),
+  peligrosidad integer,
+  CONSTRAINT intersecciones_pkey PRIMARY KEY (id),
+  CONSTRAINT intersecciones_usuario_fkey FOREIGN KEY (usuario)
+      REFERENCES public.usuario (usuario) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.intersecciones
+  OWNER TO postgres;
